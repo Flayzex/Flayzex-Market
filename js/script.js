eb3ff07db0b06925c96794ce1072c07c607ce656
Flayzex <redkiyflayzex@gmail.com>
2025-03-10 20:47:03 +0500

    Init

---
+++ js/script.js
@@ -0,0 +1,17 @@
+let starsQuantity = document.getElementById('stars-quantity');
+
+starsQuantity.addEventListener('input', function() {
+    let quantity = parseInt(starsQuantity.value); // Преобразуем в число
+
+    if (quantity < 50 || isNaN(quantity)) {
+        document.getElementById('stars-price').innerText = `Minimum 50 stars required`;
+        document.getElementById('btn-buy-stars').innerText = `Buy Telegram Stars`;
+        return; // Выходим из функции, не выполняя расчёты
+    }
+
+    let var_price = quantity * 0.015;
+    let price = (var_price + var_price * 0.1).toFixed(2);
+
+    document.getElementById('stars-price').innerText = `${price} USD or ${(price * 13250).toFixed(0)} UZS`;
+    document.getElementById('btn-buy-stars').innerText = `Buy ${quantity} Telegram Stars`;
+});
